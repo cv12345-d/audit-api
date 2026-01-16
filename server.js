b2c4a7aa11711e5b7cb0e432e2a551b9fa44f81d
cv12345-d <caroline.vromman@gmail.com>
2026-01-16 19:36:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors()); // Permet les requêtes depuis n'importe quel domaine
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Fichier de stockage des audits
@@ -158,4 +162,5 @@ initializeStorage().then(() => {
 process.on('SIGTERM', () => {
   console.log('Arrêt du serveur...');
   process.exit(0);
+
 });
